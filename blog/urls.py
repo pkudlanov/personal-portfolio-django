bc16d890acca50fe52caac65e1cffcd691a821ad
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-02-10 15:42:00 -0500

    Created detail page Empty detail page for now, urls, templates, and views Also changed a bit of naming for entries from blogs to posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.all_blogs, name='all_blogs'),
+    path('', views.all_posts, name='all_posts'),
+    path('<int:post_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
-def all_blogs(request):
+def all_posts(request):
     posts = Post.objects.order_by('-id')[:5]
-    return render(request, 'blog/all_blogs.html', {'posts': posts})
+    return render(request, 'blog/all_posts.html', {'posts': posts})
+
+
+def detail(request, post_id):
+    post = get_object_or_404(Post,  pk=post_id)
+    return render(request, 'blog/detail.html', {'post': post})
