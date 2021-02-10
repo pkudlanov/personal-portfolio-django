27dc048d410e8a9900048d07c3b81f9c1556e964
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-02-10 01:38:42 -0500

    Created the blog page, model, urls, views, templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(auto_now_add=True)
+    body = models.TextField(max_length=555)
+
+    def __str__(self) -> str:
+        return self.title
