5a2a28bee22edd0fa269d979c0e332eba37215b4
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-02-24 20:30:29 -0500

    Added likes field to the Post model

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField(auto_now_add=True)
-    body = models.TextField(max_length=555)
+    body = models.TextField()
+    likes = models.IntegerField(default=0)
 
     def __str__(self) -> str:
         return self.title
