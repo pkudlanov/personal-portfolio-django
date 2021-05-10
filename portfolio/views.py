365d7f0760ca9e0aec5484d3c3b0813f8b19068a
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-05-10 19:17:54 -0400

    Created a new send_email view in charge of sending email

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,31 +1,32 @@
 from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.http.response import JsonResponse
+from django.shortcuts import render
 
 from .models import Project
 from .forms import ContactForm
 
 
 def home(request):
-    if request.method == 'GET':
-        form = ContactForm()
-    else:
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            # name = form.cleaned_data['name']
-            subject = form.cleaned_data['subject']
-            from_email = form.cleaned_data['from_email']
-            message = form.cleaned_data['message']
-            try:
-                send_mail(subject, message, from_email, ['me@kudlanov.com'])
-            except BadHeaderError:
-                return HttpResponse('Invalid header found.')
-            messages.success(request, ('Email sent successfully.'))
-            return redirect('home')
+    form = ContactForm()
 
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects': projects, 'form': form})
+
+
+def send_email(request):
+    send_form = ContactForm(request.POST)
+    if send_form.is_valid():
+        # name = form.cleaned_data['name']
+        from_email = send_form.cleaned_data['from_email']
+        subject = send_form.cleaned_data['subject']
+        message = send_form.cleaned_data['message']
+        try:
+            send_mail(subject, message, from_email, ['me@kudlanov.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return JsonResponse({'success_message': 'Your voice is on its way to be heard by me!'})
+    return JsonResponse({'error': 'Not able to validate form'})
 
 
 def test(request):
