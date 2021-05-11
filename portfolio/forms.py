1b0ea69dd977cba23c2663ac8321332b12b64290
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-05-11 01:51:03 -0400

    Changed from_email CharField to an EmailField

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    from_email = forms.CharField(required=True)
+    from_email = forms.EmailField(required=True)
     name = forms.CharField(required=True)
     subject = forms.CharField(required=True)
     message = forms.CharField(widget=forms.Textarea, required=True)
