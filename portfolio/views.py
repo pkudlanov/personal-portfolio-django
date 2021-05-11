2d0a91de20edb92c27de7b631c16d83b0ed31475
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-05-11 01:55:48 -0400

    send_email View works with ajax Sends success_message and form_errors as json

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,6 +3,11 @@ from django.http import HttpResponse
 from django.http.response import JsonResponse
 from django.shortcuts import render
 
+import json
+
+# Including for debugging
+# from pprint import pprint
+
 from .models import Project
 from .forms import ContactForm
 
@@ -16,7 +21,9 @@ def home(request):
 
 def send_email(request):
     send_form = ContactForm(request.POST)
+    data_valid = False
     if send_form.is_valid():
+        data_valid = True
         # name = form.cleaned_data['name']
         from_email = send_form.cleaned_data['from_email']
         subject = send_form.cleaned_data['subject']
@@ -25,8 +32,15 @@ def send_email(request):
             send_mail(subject, message, from_email, ['me@kudlanov.com'])
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
-        return JsonResponse({'success_message': 'Your voice is on its way to be heard by me!'})
-    return JsonResponse({'error': 'Not able to validate form'})
+        return JsonResponse({
+            'success_message': 'Your voice is on its way to be heard by me!',
+            'data_valid': data_valid
+        })
+    return JsonResponse({
+        'success_message': 'Not able to validate form',
+        'form_errors': json.dumps(send_form.errors),
+        'data_valid': data_valid
+    })
 
 
 def test(request):
