42336f89aa1ba62235509b5cabaf7c313e65fbef
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-05-30 21:21:26 -0400

    Running collectstatic in g-actions as well

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -134,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = env('STATIC_ROOT')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = env('MEADIA_ROOT')
 
 try:
     from .local_settings import *  # noqa: F403 F401
