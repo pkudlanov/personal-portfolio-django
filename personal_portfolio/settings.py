c577161c7580a9745c1f8d6d4261fedd49ae8717
Pavel Kudlanov <pkudlanov@yahoo.com>
2021-05-31 14:26:52 -0400

    In settings declared specific path of .env file

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -13,7 +13,8 @@ import os
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
+ENV_PATH = env.str('ENV_PATH')
+environ.Env.read_env(ENV_PATH)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
