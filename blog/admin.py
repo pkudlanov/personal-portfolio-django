5053307ff48d6ca457d9eea3c467b1350364878b
Pavel Kudlanov <pkudlanov@yahoo.com>
2022-04-01 12:02:58 -0400

    Changed Size of textarea in django admin It was just annoyingly small for writing blog posts Gave it small size in admin.py but should be editable now by dragging

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.db import models
-from django.forms import CheckboxSelectMultiple
+from django.forms import CheckboxSelectMultiple, Textarea
 from .models import Post, Category
 
 
@@ -8,6 +8,7 @@ class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('date',)
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+        models.TextField: {'widget': Textarea(attrs={'rows':15, 'cols':100})},
     }
 
 
