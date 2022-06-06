857e586ec39fe25984e3af4623b789b4ae34f534
Pavel Kudlanov <pkudlanov@yahoo.com>
2022-06-06 15:37:38 -0400

    Comment model post field updated with related name

--- blog/models.py
+++ blog/models.py
@@ -32,7 +32,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=64)
     email = models.EmailField(max_length=64)
